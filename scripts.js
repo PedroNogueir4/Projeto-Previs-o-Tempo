2989c6a164378de65a8001a5dee9374733adb227
PedroNogueir4 <pedroffn209@gmail.com>
2023-03-31 19:16:26 -0300

    Aplicação Finalizada

--- scripts.js
+++ scripts.js
@@ -1,8 +1,24 @@
- const key = "28668df08bb504525fea307a64624112"
+const key = "28668df08bb504525fea307a64624112"
 
- function buscarCidade () {
+function dataInput() {
 
-const input = document.querySelector(".search").value
-console.log(input)
+    const city = document.querySelector(".search").value
 
- }
+    searchCity(city)
+}
+
+async function searchCity(city) {
+    const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`).then(response => response.json())
+
+    dataScreen(data)
+}
+
+
+function dataScreen(data) {
+
+    document.querySelector(".name-city").innerHTML = `Tempo em ${data.name}`
+    document.querySelector(".humidity").innerHTML = `Umidade:  ${data.main.humidity}%`
+    document.querySelector(".weather").innerHTML = `${data.weather[0].description}`
+    document.querySelector(".temp").innerHTML = Math.floor(data.main.temp)+"°C"
+
+}
